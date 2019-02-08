587ab7ddc308446d9c5e4edf450f050e97e5f4bf
Henry <pienry3g@gmail.com>
2019-02-08 15:34:15 +0100

    Update src/utils/getStoreState.js

--- src/utils/getStoreState.js
+++ src/utils/getStoreState.js
@@ -3,22 +3,10 @@ import { setCurrentCart } from '../actions/cartActions';
 import { setAxiosToken } from './axiosInstance';
 
 export default (store) => {
-  if (localStorage.userToken && localStorage.userId) {
-    const {
-      fullname = '', role = 'User', userId = '', userToken = '',
-    } = localStorage;
-
+  if (localStorage.userToken) {
     setAxiosToken(localStorage.userToken);
 
-    store.dispatch(
-      setAuthUser({
-        isAuthenticated: true,
-        fullname,
-        role,
-        userId,
-        userToken,
-      }),
-    );
+    store.dispatch(setAuthUser(localStorage.userToken));
   }
 
   const foodItems = localStorage.foodItems ? JSON.parse(localStorage.foodItems) : [];
